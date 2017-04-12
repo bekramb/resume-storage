8864dd3698e65d874e90eaaeffd01bfbfe1001aa
me <bekramb@gmail.com>
2017-04-13 00:47:06 +0300

    Second lesson homework. Create update method.

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,45 +1,66 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
 /**
  * Array based storage for Resumes
  */
 
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
     private int size;
 
-    void clear() {
+    public void clear() {
         size = 0;
     }
 
-    void save(Resume r) {
-        if (getIndex(r.uuid) == -1 && r.uuid != null) {
+    public void save(Resume r) {
+        if (getIndex(r.getUuid()) == -1) {
             if (size <= storage.length) {
                 storage[size] = r;
                 size++;
+            }else{
+                System.out.println("Storage is full!");
             }
+        }else{
+            System.out.println("Resume with such uuid already exists!");
         }
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         int index = getIndex(uuid);
         Resume r = null;
         if (index != -1) {
             r = storage[index];
+        }else{
+            System.out.println("There is no resume with such uuid");
         }
         return r;
     }
 
-    void delete(String uuid) {
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index != -1) {
+            storage[index] = r;
+        }else{
+            System.out.println("There is no resume with such uuid");
+        }
+    }
+
+    public void delete(String uuid) {
         int index = getIndex(uuid);
         if (index != -1) {
             System.arraycopy(storage, index + 1, storage, index, size - index);
             size--;
+        }else{
+            System.out.println("There is no resume with such uuid");
         }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         Resume[] arr = new Resume[size];
         for (int i = 0; i < size; i++) {
             arr[i] = storage[i];
@@ -47,23 +68,21 @@ public class ArrayStorage {
         return arr;
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 
     /**
      * @param uuid
-     * @return index if storage contains Resume with uuid otherwise -1
+     * @return index if storage contains com.urise.webapp.model.Resume with uuid otherwise -1
      */
-
-
-    int getIndex(String uuid) {
+    public int getIndex(String uuid) {
         int index = -1;
         if (null == uuid || uuid.isEmpty()){
             return index;
         }
         for (int i = 0; i < size; i++) {
-            if (uuid.equals(storage[i].uuid)) {
+            if (uuid.equals(storage[i].getUuid())) {
                 index = i;
                 break;
             }
--- src/svidersky/andrey/Main.java
+++
@@ -1,8 +0,0 @@
-package svidersky.andrey;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
